0ae9935dccd496120b4fce2f6b9131bc3fd4a6cb
James Horton <jameshorton@gmail.com>
2018-09-20 10:51:17 -0400

    completed up to step 4

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
+let dest = document.getElementById('mainWrapper');
 class Grid {
     constructor(options){
-        let arrayOfColumns = []
+        let gridArray = [];
+        this.arrayOfColumns = gridArray;
+        this.name = options.name || 'grid';
         this.numberOfColumns = options.numberOfColumns;
         this.numberOfRows = options.numberOfRows;
         this.borderColor = options.borderColor || 'black';
         this.borderThickness = options.borderThickness || '1px';
+        let wrapper = document.createElement('div');
+        wrapper.id = options.name + 'Div';
+        wrapper.className = 'grid';
         for (let c = 0; c < options.numberOfColumns; c++){
             let column = [];
+            let columnDiv = document.createElement('div');
+            columnDiv.id = `column${c}`;
+            columnDiv.class = 'column';
             for (let r = 0; r < options.numberOfRows; r++){
                 const cell = new Cell(c,r);
+                let cellDiv = cell.createCellDiv();
                 column.push(cell);
+                columnDiv.appendChild(cellDiv)
             }
-            arrayOfColumns.push(column);
+            gridArray.push(column);
+            wrapper.appendChild(columnDiv);
         }
-        return arrayOfColumns;
+        dest.appendChild(wrapper);
+        // return arrayOfColumns;
     }
 }
 
@@ -22,6 +35,8 @@ class Cell {
         this.columnNumber = columnNumber;
         this.rowNumber = rowNumber;
         this.divID = `cellAtColumn${columnNumber}Row${rowNumber}`;
+    }
+    createCellDiv(){
         let cell = document.createElement('div');
         cell.id = this.divID;
         cell.className = 'cell';
@@ -31,7 +46,11 @@ class Cell {
 
 const grid1 = new Grid ({
     numberOfColumns: 3,
-    numberOfRows: 4
+    numberOfRows: 4,
+    name: 'superSweetGrid'
 })
 
-console.log(grid1);
+console.log(grid1.arrayOfColumns);
+
+// const cell1 = new Cell(1,2);
+// console.log(cell1);
